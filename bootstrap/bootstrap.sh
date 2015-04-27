3d755ef90f0e21bb0f943b02d4352b74d783c35e
Mike Kasprzak <mike@sykhronics.com>
2015-04-27 13:28:05 -0400

    Removed the root test

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 # This is the bootstrapping script run to provision the Vagrant install of Scotch Box.
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
 # To correctly use pecl and other packages, we need a few prerequisites
 apt-get update
 printf "y" | apt-get install php5-dev
--- bootstrap/install-apcu.sh
+++ bootstrap/install-apcu.sh
@@ -2,11 +2,6 @@
 # NOTE: This script is separate because something was causing it to execute out-of order.
 #       Restarting Apache before I finish installing is dumb. Separating fixed it.
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
 echo "Installing APCu..."
 printf "no\nno" | pecl install APCu-beta
 echo "extension=apcu.so">>/etc/php5/apache2/php.ini
