377e43b4e143012de665d0e9f50f94fd4427c296
Mike Kasprzak <mike@sykhronics.com>
2015-04-29 14:45:50 -0400

    corrected how globals work

--- www/config.php
+++ www/config.php
@@ -1,5 +1,10 @@
 <?php
 
+global $cfg_db_host;
+global $cfg_db_name;
+global $cfg_db_login;
+global $cfg_db_password;
+
 $cfg_db_host = "localhost";
 $cfg_db_name = "scotchbox";
 $cfg_db_login = "root";
