83c6facd0ff7031bb958f953f004b56185cc292b
Mike Kasprzak <mike@sykhronics.com>
2015-04-29 15:47:49 -0400

    better default config

--- www/config.php
+++ www/config.php
@@ -1,13 +1,12 @@
 <?php
 
-global $cfg_db_host;
-global $cfg_db_name;
-global $cfg_db_login;
-global $cfg_db_password;
-
+// Database Configuration //
 $cfg_db_host = "localhost";
 $cfg_db_name = "scotchbox";
 $cfg_db_login = "root";
 $cfg_db_password = "root";
+
+// By default we use MySQL. Enable this flag to use PDO. //
+//$cfg_uses_pdo = true;
 
 ?>
