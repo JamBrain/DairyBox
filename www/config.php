34bc7d72ff4647561fe444c031ca7ffdc938e6a9
Mike Kasprzak <mike@sykhronics.com>
2015-05-02 11:53:29 -0400

    added a stub for debug mode to the config

--- www/config.php
+++ www/config.php
@@ -6,6 +6,11 @@ $cfg_db_name = "scotchbox";
 $cfg_db_login = "root";
 $cfg_db_password = "root";
 
+
+// Debug Mode - Outputs extra information //
+//$cfg_debug = true;
+
+
 // By default we use MySQL. Enable this flag to use PDO. //
 //$cfg_uses_pdo = true;
 
