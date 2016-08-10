35769beae98a24aad1fdd24aa2b0dfdd99cd65cb
Mike Kasprzak <mike@sykhronics.com>
2016-08-10 13:06:32 -0400

    auto installing PHPMyAdmin, moved the dev tools

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -27,12 +27,24 @@ apt-get -y install ffmpeg imagemagick pngquant gifsicle webp
 #apt-get -y remove mysql-server
 #apt-get -y autoremove
 
-apt-get -y install software-properties-common
-apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
-add-apt-repository -y 'deb [arch=amd64,i386] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.1/ubuntu trusty main'
+#apt-get -y install software-properties-common
+#apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
+#add-apt-repository -y 'deb [arch=amd64,i386] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.1/ubuntu trusty main'
 
-apt-get update
+#apt-get update
 #printf "\n\n" | apt-get -y install mariadb-server
+
+# PHPMyAdmin
+if [ ! -d "/vagrant/www/sandbox/phpmyadmin" ]; then
+	wget --quiet https://files.phpmyadmin.net/phpMyAdmin/4.6.3/phpMyAdmin-4.6.3-all-languages.zip
+	unzip phpMyAdmin-4.6.3-all-languages.zip
+	sudo mv phpMyAdmin-4.6.3-all-languages /vagrant/www/sandbox/phpmyadmin
+	rm phpMyAdmin-4.6.3-all-languages.zip
+fi
+
+# NodeJS CSS dependencies
+#npm install -g del
+#npm install --prefix /vagrant/www gulp gulp-postcss gulp-sourcemaps autoprefixer cssnano gulp-less stylelint
 
 # Run codebase specific setup scripts
 sh /var/www/scripts/setup.sh || true
@@ -42,6 +54,7 @@ ln -s /var/www www
 ln -s /vagrant/www vvv
 ln -s /vagrant/provision/start_mailcatcher.sh start_mailcatcher.sh
 ln -s /etc/php5/apache2/php.ini php.ini
+ln -s /etc/php5/apache2/conf.d/user.ini user.ini
 ln -s /etc/apache2/apache2.conf apache2.conf
 ln -s /etc/mysql/my.cnf mysql.conf
 ln -s /etc/redis/6379.conf redis.conf
