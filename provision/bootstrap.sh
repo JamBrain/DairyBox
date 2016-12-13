ee7ef2ad1429def9bdbeafec3e731c0bbed4cb4e
Mike Kasprzak <mike@sykhronics.com>
2016-12-13 01:10:13 -0500

    Should let toolchain build out-of-the-box

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -9,49 +9,60 @@ add-apt-repository -y ppa:mc3man/trusty-media
 # Newer version of ImageMagick on Ubuntu 14.04 (needed for correct webp support)
 add-apt-repository -y ppa:jamedjo/ppa
 
-# To correctly use pecl and other packages, we need a few prerequisites
+# Repo for PHP 7.0 (for Ubuntu 14.04)
+add-apt-repository -y ppa:ondrej/php
+
+# Repo for MariaDB 10.1 (for Ubuntu 14.04)
+apt-get install -y software-properties-common
+apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
+add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://mirrors.accretive-networks.net/mariadb/repo/10.1/ubuntu trusty main'
+
+# Update once after all new repos have been added
 apt-get update
-apt-get -y install php5-dev
+
+# Install packages
+apt-get -y install ffmpeg imagemagick pngquant gifsicle webp php7.0 php7.0-mbstring php7.0-mysql php7.0-xml php-apcu
+
+# Switch Apache to PHP 7.0
+a2dismod php5
+a2enmod php7.0
+
+# To correctly use pecl and other packages, we need a few prerequisites
+#apt-get -y install php7.0-dev
 
 # Tell Pear/Pecl packages where to find php.ini
-pear config-set php_ini /etc/php5/apache2/php.ini
+#pear config-set php_ini /etc/php5/apache2/php.ini
 
 # Install PHP packages
-echo "Installing APCu..."
-printf "no\nno" | pecl install APCu-4.0.11
+#echo "Installing APCu..."
+#printf "no\nno" | pecl install APCu-4.0.11
 
-# Install other packages
-apt-get -y install ffmpeg imagemagick pngquant gifsicle webp
-
-# MariaDB
-#apt-get -y remove mysql-server
-#apt-get -y autoremove
-
-#apt-get -y install software-properties-common
-#apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
-#add-apt-repository -y 'deb [arch=amd64,i386] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.1/ubuntu trusty main'
-
-#apt-get update
-#printf "\n\n" | apt-get -y install mariadb-server
+# Install MariaDB
+apt-get -y remove mysql-server
+apt-get -y autoremove
+DEBIAN_FRONTEND=noninteractive apt-get -y install mariadb-server
 
 # PHPMyAdmin
-if [ ! -d "/vagrant/www/sandbox/phpmyadmin" ]; then
+if [ ! -d "/vagrant/dev/phpmyadmin" ]; then
 	wget --quiet https://files.phpmyadmin.net/phpMyAdmin/4.6.3/phpMyAdmin-4.6.3-all-languages.zip
 	unzip phpMyAdmin-4.6.3-all-languages.zip
-	sudo mv phpMyAdmin-4.6.3-all-languages /vagrant/www/sandbox/phpmyadmin
+	mv phpMyAdmin-4.6.3-all-languages /vagrant/dev/phpmyadmin
 	rm phpMyAdmin-4.6.3-all-languages.zip
 fi
 
 # NodeJS CSS dependencies
-#npm install -g del
+npm install -p /vagrant/www
+npm install -g svgo less clean-css buble rollup uglify-js
+
 #npm install --prefix /vagrant/www gulp gulp-postcss gulp-sourcemaps autoprefixer cssnano gulp-less stylelint
 
 # Run codebase specific setup scripts
 sh /var/www/scripts/setup.sh || true
 
 # Create symlinks to useful folders
-ln -s /var/www www
-ln -s /vagrant/www vvv
+#ln -s /var/www www
+#ln -s /vagrant/www vvv
+ln -s /vagrant/www www
 ln -s /vagrant/provision/start_mailcatcher.sh start_mailcatcher.sh
 ln -s /etc/php5/apache2/php.ini php.ini
 ln -s /etc/php5/apache2/conf.d/user.ini user.ini
