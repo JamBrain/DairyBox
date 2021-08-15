ca5eba8f7a432d30f9ce85226e0f7074bfcd6101
Mike Kasprzak <mikekasprzak+x270@gmail.com>
2021-08-14 20:36:07 -0400

    v16 works too

--- provision/userbootstrap.sh
+++ provision/userbootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is the bootstrapping script that provisions the vagrant user
 
-NODEJS_VERSION="v14"
+NODEJS_VERSION="v16"
 
 echo "\nRunning userbootstrap.sh as unprivileged user \n"
 
